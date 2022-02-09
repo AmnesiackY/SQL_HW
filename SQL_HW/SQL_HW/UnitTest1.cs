987864320a1a5aebd62211ffad1dd1b2f192cce9
AmnesiackY <hitsac2711@gmail.com>
2022-02-10 00:17:58 +0200

    ver 2.0 end minimal quantity of

--- SQL_HW/SQL_HW/UnitTest1.cs
+++ SQL_HW/SQL_HW/UnitTest1.cs
@@ -7,36 +7,36 @@ namespace SQL_HW
 {
     public class UnitTest1
     {
-        List<string> studentName = new List<string>();
-        List<string> companyName = new List<string>();
-        List<string> softSkills = new List<string>();
-        List<string> hardSkills = new List<string>();
+        List<string> department = new List<string>();
+        List<string> user_id = new List<string>();
 
         [Fact]
         public void Test1()
         {
-            List<string> fullnames = new List<string>();
-            List<int> user_id = new List<int>();
             SQLiteConnection db = SQL_Connection.Connect(@"Data Source=C:/Users/hitsa/Desktop/SQL_HW/base_SQL.db");
-            SQLiteCommand cmd = SQL_Connection.SQLCommand(db, "Select * FROM Persons");
-            //var obj = cmd.ExecuteScaler();
-            SQLiteDataReader reader = cmd.ExecuteReader();
-            //заполняем списки датой
-            while (reader.Read())
-            {
-                fullnames.Add(reader[0].ToString());
-                user_id.Add(Convert.ToInt32(reader[1]));
-            }
-            //вывод инфы в консоль
-            foreach (string a in fullnames)
-            {
-                Console.WriteLine(a);
-            }
-            foreach (int a in user_id)
-            {
-                Console.WriteLine(a);
-            }
-            Console.ReadKey();
+            SQLiteCommand cmd = SQL_Connection.SQLCommand(db, "SELECT Department FROM QA_Course_Students WHERE Student_ID = (SELECT Student_ID FROM Lectures WHERE Theme = 'Git' AND Activity = 10) ");
+            var obj = cmd.ExecuteScalar();
+            Assert.Equal("Y-Tree", (obj).ToString());
+            SQL_Connection.CloseConnect(db);
         }
+        [Fact]
+        public void Test2()
+        {
+            SQLiteConnection db = SQL_Connection.Connect(@"Data Source=C:/Users/hitsa/Desktop/SQL_HW/base_SQL.db");
+            SQLiteCommand cmd = SQL_Connection.SQLCommand(db, "SELECT Full_Name FROM QA_Course_Students WHERE Student_ID = (SELECT Student_ID FROM Lectures WHERE Theme = 'SQL' AND Activity = 5) ");
+            var obj = cmd.ExecuteScalar();
+            Assert.Equal("Shugay Katya", obj.ToString());
+            SQL_Connection.CloseConnect(db);
+        }
+        [Fact]
+        public void Test3()
+        {
+            SQLiteConnection db = SQL_Connection.Connect(@"Data Source=C:/Users/hitsa/Desktop/SQL_HW/base_SQL.db");
+            SQLiteCommand cmd = SQL_Connection.SQLCommand(db, "SELECT Soft_Skills FROM QA_Course_Students WHERE Student_ID = (SELECT Student_ID FROM Lectures WHERE Theme = 'Mobile Testing' AND Activity = 6) ");
+            var obj = cmd.ExecuteScalar();
+            Assert.Equal(88,Convert.ToInt32(obj));
+            SQL_Connection.CloseConnect(db);
+        }
+
     }
 }
