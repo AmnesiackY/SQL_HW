96bdc17dda9f49196434abc4354f24501ea69648
AmnesiackY <hitsac2711@gmail.com>
2022-02-10 12:36:10 +0200

    ver 2.1 release

--- SQL_HW/SQL_HW/UnitTest1.cs
+++ SQL_HW/SQL_HW/UnitTest1.cs
@@ -7,9 +7,6 @@ namespace SQL_HW
 {
     public class UnitTest1
     {
-        List<string> department = new List<string>();
-        List<string> user_id = new List<string>();
-
         [Fact]
         public void Check_DepartmentByID()
         {
@@ -37,6 +34,43 @@ namespace SQL_HW
             Assert.Equal(88,Convert.ToInt32(obj));
             SQL_Connection.CloseConnect(db);
         }
-
+        [Fact]
+        public void Check_StudentNameByLectureDepartment()
+        {
+            List<string> fullnames = new List<string>();
+            SQLiteConnection db = SQL_Connection.Connect(@"Data Source=C:/Users/hitsa/Desktop/SQL_HW/base_SQL.db");
+            SQLiteCommand cmd = SQL_Connection.SQLCommand(db, "SELECT Full_Name FROM QA_Course_Students WHERE Department = (SELECT Department FROM QA_Course_Lectures WHERE Full_Name = 'Bogdan Kryvko' ) ");
+            SQLiteDataReader reader = cmd.ExecuteReader();  
+            while (reader.Read())
+            {
+                fullnames.Add(reader[0].ToString());
+            }
+            Assert.Equal("Yarovoy Yaroslav", fullnames[0]); 
+            SQL_Connection.CloseConnect(db);
+        }
+        [Fact]
+        public void Check_StudentIDByLectureDepartment()
+        {
+            List<string> fullnames = new List<string>();
+            SQLiteConnection db = SQL_Connection.Connect(@"Data Source=C:/Users/hitsa/Desktop/SQL_HW/base_SQL.db");
+            SQLiteCommand cmd = SQL_Connection.SQLCommand(db, "SELECT Student_ID FROM QA_Course_Students WHERE Department = (SELECT Department FROM QA_Course_Lectures WHERE Full_Name = 'Bogdan Kryvko' ) ");
+            SQLiteDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                fullnames.Add(reader[0].ToString());
+            }
+            Assert.Equal("4", fullnames[1]);
+            SQL_Connection.CloseConnect(db);
+        }
+        [Fact]
+        public void Check_LectureNameByStudentName()
+        {
+            List<string> fullnames = new List<string>();
+            SQLiteConnection db = SQL_Connection.Connect(@"Data Source=C:/Users/hitsa/Desktop/SQL_HW/base_SQL.db");
+            SQLiteCommand cmd = SQL_Connection.SQLCommand(db, "SELECT Full_Name FROM QA_Course_Lectures WHERE Lecture_ID = (SELECT Lecture_ID FROM Lectures WHERE Student_ID = (SELECT Student_ID FROM QA_Course_Students WHERE Full_Name = 'Yarovoy Yaroslav'))");
+            var obj = cmd.ExecuteScalar();
+            Assert.Equal("Bogdan Kryvko", obj);
+            SQL_Connection.CloseConnect(db);
+        }
     }
 }
